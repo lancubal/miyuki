c0865d72a1a5bcf9c8f0c8f8d75803c148d9c960
Diana Lopez Alvas <dlopezalvas@gmail.com>
2026-01-17 19:42:25 -0300

    Migracion de CRA a Vite

--- miyuki-laboratory/src/index.tsx
+++ miyuki-laboratory/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'dev-awesome/dist/css/dev-awesome.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
